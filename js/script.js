f51e26c5d9d7a3931907d16b2093af7896e49ed7
MaryAug <martmary566@gmail.com>
2025-02-07 12:54:42 +0300

    html/css header

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// let dropdowns = document.querySelectorAll(".menu-services__item");
+// for (let dropdown of dropdowns) {
+//   dropdown.addEventListener("mouseover", function () {
+//     this.firstElementChild.addClass("submenu--active");
+//     this.firstElementChild.removeClass("submenu");
+//   });
+// }
