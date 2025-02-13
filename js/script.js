125d787feebdb5818b7f289c5851124514e0b68e
MaryAug <martmary566@gmail.com>
2025-02-13 13:35:58 +0300

    HTML layout

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@
 //   });
 // }
 
+// Бургер-меню
 const burgerBtn = document.querySelector(".menu-trigger");
 const menu = document.querySelector(".menu");
 burgerBtn.addEventListener("click", (e) => {
@@ -27,3 +28,25 @@ burgerBtn.addEventListener("click", (e) => {
     }
   });
 });
+
+// Аккордеон
+function accordion() {
+  const items = document.querySelectorAll(".accordion__item-trigger");
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      const parent = item.parentNode;
+      if (parent.classList.contains("accordion__item--active")) {
+        parent.classList.remove("accordion__item--active");
+      } else {
+        document
+          .querySelectorAll(".accordion__item")
+          .forEach((child) =>
+            child.classList.remove("accordion__item--active")
+          );
+        parent.classList.add("accordion__item--active");
+      }
+    });
+  });
+}
+
+accordion();
